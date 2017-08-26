cdd5d930de3d49b487b0dde3b6468d49856a088b
Alex Kamsteeg <alex@atlex.nl>
2017-08-26 13:14:06 +0200

    Added basic benchmark discovery and running

--- src/BenchmarkDotNetRunner/Program.cs
+++ src/BenchmarkDotNetRunner/Program.cs
@@ -1,5 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 using AtleX.CommandLineArguments;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
 using BenchmarkDotNetRunner.Cli;
 
 namespace BenchmarkDotNetRunner
@@ -18,7 +25,11 @@ namespace BenchmarkDotNetRunner
       {
         try
         {
+          var config = CreateConfig(arguments);
+          var benchmarks = GetBenchmarks(arguments.BenchmarkAssembly, config) as Benchmark[];
 
+
+          BenchmarkRunner.Run(benchmarks, config);
         }
         catch (Exception e)
         {
@@ -30,6 +41,50 @@ namespace BenchmarkDotNetRunner
       }
 
       return (int)result;
+    }
+
+    private static IConfig CreateConfig(CliArguments arguments)
+    {
+      var config = ManualConfig.Create(DefaultConfig.Instance);
+      config.Add(Job.Default);
+
+      if (arguments.EnableMemoryDiagnoser)
+      {
+        var gcDiagnoser = new MemoryDiagnoser();
+        config.Add(gcDiagnoser);
+      }
+
+      return config;
+    }
+
+    private static IEnumerable<Benchmark> GetBenchmarks(string assemblyFileName, IConfig benchmarkConfiguration)
+    {
+      List<Benchmark> result = new List<Benchmark>();
+
+      var benchmarkAssembly = Assembly.LoadFile(assemblyFileName);
+
+      var exportedTypesInAssembly = benchmarkAssembly.GetExportedTypes();
+
+      foreach (var currentType in exportedTypesInAssembly)
+      {
+        var methodsInType = currentType.GetMethods(BindingFlags.Public);
+
+        foreach (var currentMethod in methodsInType)
+        {
+          if (currentMethod.GetCustomAttribute<BenchmarkAttribute>() != null)
+          {
+            var target = new Target(currentType, currentMethod);
+
+            foreach (var currentJob in benchmarkConfiguration.GetJobs())
+            {
+              var benchmark = new Benchmark(target, currentJob, null);
+              result.Add(benchmark);
+            }
+          }
+        }
+      }
+
+      return result;
     }
   }
 }
